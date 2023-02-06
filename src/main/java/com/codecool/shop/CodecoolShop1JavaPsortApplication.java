84f42bab6ede2bdaaa6cf235d1ece27215d1bad8
Psort <skowronpiotr.00@gmail.com>
2023-02-06 14:23:58 +0100

    add Spring maven file

---
+++ src/main/java/com/codecool/shop/CodecoolShop1JavaPsortApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecoolShop1JavaPsortApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodecoolShop1JavaPsortApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codecoolshop1javapsort/CodecoolShop1JavaPsortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codecoolshop1javapsort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodecoolShop1JavaPsortApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
