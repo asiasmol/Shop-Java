b5239d4514db02d2774045c0876f7ee3bdc574f8
Tomasz Kraczek <tomek6363@gmail.com>
2023-02-21 11:48:55 +0100

    First part of productService refactor

--- src/main/java/com/codecool/shop/service/ProductService.java
+++ src/main/java/com/codecool/shop/service/ProductService.java
@@ -2,18 +2,24 @@ package com.codecool.shop.service;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ProductService {
     private ProductDao productDao;
     private ProductCategoryDao productCategoryDao;
+    private final SupplierDao supplierDao;
 
-    public ProductService(ProductDao productDao, ProductCategoryDao productCategoryDao) {
+    public ProductService(ProductDao productDao, ProductCategoryDao productCategoryDao, SupplierDao supplierDao) {
         this.productDao = productDao;
         this.productCategoryDao = productCategoryDao;
+        this.supplierDao = supplierDao;
     }
 
     public ProductCategory getProductCategory(int categoryId) {
@@ -25,5 +31,19 @@ public class ProductService {
         return productDao.getBy(category);
     }
 
+    public List<Product> getAll() {
+        return productDao.getAll();
+    }
 
+    public List<Product> getByCategoryId(int categoryId) {
+        return productDao.getBy(productCategoryDao.find(categoryId));
+    }
+
+    public List<Product> getBySuppliers(int suppliersId) {
+        return productDao.getBy(supplierDao.find(suppliersId));
+    }
+
+    public List<Supplier> getAllSuppliers() {
+        return supplierDao.getAll();
+    }
 }
--- src/main/java/com/codecool/shop/web/ProductController.java
+++ src/main/java/com/codecool/shop/web/ProductController.java
@@ -9,6 +9,7 @@ import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import com.codecool.shop.model.Cart;
 import com.codecool.shop.model.Product;
+import com.codecool.shop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,74 +22,72 @@ import java.util.Optional;
 
 @Controller
 public class ProductController {
-    private final ProductDao productDao;
-    private final ProductCategoryDao productCategoryDao;
-    private final SupplierDao supplierDao;
-    private final Cart cart;
+//    private final ProductDao productDao;
+//    private final ProductCategoryDao productCategoryDao;
+//    private final SupplierDao supplierDao;
+//    private final Cart cart;
+    private final ProductService productService;
 
 
     @Autowired
-    public ProductController(Cart cart, ProductDaoMem productDao, ProductCategoryDaoMem productCategoryDao, SupplierDaoMem supplierDao, CartDaoMem cartDao) {
-        this.productDao = productDao;
-        this.productCategoryDao = productCategoryDao;
-        this.supplierDao = supplierDao;
-        this.cart = cart;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
 
     @GetMapping("/")
-    public String helloword(Model model) {
-        model.addAttribute("products",productDao.getAll());
+    public String helloWorld(Model model) {
+        model.addAttribute("products", productService.getAll());
         return basicPage(model);
     }
 
     private String basicPage(Model model) {
-        model.addAttribute("categories", productCategoryDao.getAll());
-        model.addAttribute("suppliers", supplierDao.getAll());
-        model.addAttribute("totalPrice", cart.getSum());
-        model.addAttribute("cartItems", cart.getAll());
-        model.addAttribute("counterProducts", cart.getAll().size());
-        model.addAttribute("totalPrice", cart.getSum());
+        model.addAttribute("categories", productService.getAll());
+        model.addAttribute("suppliers", productService.getAllSuppliers());
+//        model.addAttribute("totalPrice", cart.getSum());
+//        model.addAttribute("cartItems", cart.getAll());
+//        model.addAttribute("counterProducts", cart.getAll().size());
+//        model.addAttribute("totalPrice", cart.getSum());
         return "index";
     }
 
     @PostMapping("/category")
     public String sortByCategory(@RequestParam("categoryId") int categoryId, Model model) {
-        model.addAttribute("products",productDao.getBy(productCategoryDao.find(categoryId)));
+        model.addAttribute("products", productService.getByCategoryId(categoryId));
         return basicPage(model);
     }
 
     @PostMapping("/supplier")
     public String sortBySuppliers(@RequestParam("suppliersId") int suppliersId, Model model) {
-        model.addAttribute("products",productDao.getBy(supplierDao.find(suppliersId)));
+        model.addAttribute("products",productService.getBySuppliers(suppliersId));
         return basicPage(model);
     }
 
-    @GetMapping("/add/{id}")
-    public String addProductToCart(@PathVariable("id") int id, Model model) {
-        Optional<Product> oProduct = Optional.ofNullable(productDao.find(id));
-        if(oProduct.isPresent()){
-            Product product = oProduct.get();
-            cart.add(product);
-        }
-        return "redirect:/";
-    }
-
-    @GetMapping("/remove/{id}")
-    public String removeProductToCart(@PathVariable("id") int id, Model model) {
-        Optional<Product> oProduct = Optional.ofNullable(productDao.find(id));
-        if(oProduct.isPresent()){
-            Product product = oProduct.get();
-            cart.remove(product);
-        }
-        return "redirect:/";
-    }
-
-    @GetMapping("/clear")
-    public String clearCart(Model model){
-        this.cart.clearCart();
-        return "redirect:/";
-
-    }
+//    @GetMapping("/add/{id}")
+//    public String addProductToCart(@PathVariable("id") int id, Model model) {
+//        Optional<Product> oProduct = Optional.ofNullable(productDao.find(id));
+//        if(oProduct.isPresent()){
+//            Product product = oProduct.get();
+//            cart.add(product);
+//        }
+//        return "redirect:/";
+//    }
+//
+//    @GetMapping("/remove/{id}")
+//    public String removeProductToCart(@PathVariable("id") int id, Model model) {
+//        Optional<Product> oProduct = Optional.ofNullable(productDao.find(id));
+//        if(oProduct.isPresent()){
+//            Product product = oProduct.get();
+//            cart.remove(product);
+//        }
+//        return "redirect:/";
+//    }
+//
+//    @GetMapping("/clear")
+//    public String clearCart(Model model){
+//        this.cart.clearCart();
+//        return "redirect:/";
+//
+//    }
 
 
 }
