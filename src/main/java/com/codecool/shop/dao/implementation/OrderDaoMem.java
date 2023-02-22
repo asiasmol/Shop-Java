7b86b5ae333ecff68f3bd30f319deea5480a2e82
asiasmol <joanna.smolinska.cool@gmail.com>
2023-02-22 10:06:29 +0100

    progress with bugs

--- src/main/java/com/codecool/shop/dao/implementation/OrderDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/OrderDaoMem.java
@@ -3,11 +3,13 @@ package com.codecool.shop.dao.implementation;
 import com.codecool.shop.dao.OrderDao;
 import com.codecool.shop.model.Order;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+@Repository
 public class OrderDaoMem  implements OrderDao{
 
     private final JdbcTemplate jdbc;
@@ -21,6 +23,11 @@ public class OrderDaoMem  implements OrderDao{
     public void add(Order order) {
         return jdbc.query("INSERT INTO orders VALUES id = ?, user_id = ?, products = ?, data = ?, final_price = ?, is_paid = ?",
                 OrderDaoMem::mapRow, order.getId(), order.getUser().getId(), order.getProducts(), order.getDateOfOrder(), order.getFinalPrice(), order.isPaid());
+    }
+
+    @Override
+    public List<Order> getOrdersBy(int user_id) {
+        return null;
     }
 
     @Override
--- src/main/java/com/codecool/shop/web/OrderController.java
+++ src/main/java/com/codecool/shop/web/OrderController.java
@@ -4,11 +4,17 @@ import com.codecool.shop.model.Order;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.service.CartService;
 import com.codecool.shop.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
+@Controller
 public class OrderController {
 
 
@@ -16,19 +22,22 @@ public class OrderController {
     private final CartService cartService;
     private final OrderService orderService;
 
+    @Autowired
     public OrderController(CartService cartService, OrderService orderService) {
         this.cartService = cartService;
         this.orderService = orderService;
     }
 
-//    @GetMapping("/order_history/{id}")
-//    public String addOrderToHistory(@PathVariable("id") int id) {
-//        Optional<Order> oOrder = Optional.ofNullable(orderService.);
-//        if(oOrder.isPresent()){
-//            Order order = oOrder.get();
-//            order.add();
-//        }
-//        return "redirect:/";
-//    }
+    @GetMapping("/order_history/{id}")
+    public String addOrderToHistory(@PathVariable("id") int id) {
+        Optional<Order> oOrder = Optional.ofNullable(orderService.);
+        if(oOrder.isPresent()){
+            Order order = oOrder.get();
+            order.add();
+        }
+        return "redirect:/";
+    }
+
+
 
 }
